738a397219169a46557e12d4dd4adc08fc701e7d
Miles Le <eric.nguyen@fleishman.com>
2021-05-12 11:59:00 -0500

    Refactor slot reels and slot machine

--- src/components/SlotMachine/SlotMachine.vue
+++ src/components/SlotMachine/SlotMachine.vue
@@ -1,18 +1,115 @@
 <template>
   <v-card class="d-flex justify-center mb-6" flat tile>
-    <div class="SlotMachine"></div>
+    <div class="d-flex flex-column">
+      <div class="SlotMachine d-flex">
+        <Reels
+          :animate="animate"
+          ref="reel1"
+          v-on:stopped="reelStopped"
+        ></Reels>
+        <Reels
+          :animate="animate"
+          ref="reel2"
+          v-on:stopped="reelStopped"
+        ></Reels>
+        <Reels
+          :animate="animate"
+          ref="reel3"
+          v-on:stopped="reelStopped"
+        ></Reels>
+      </div>
+      <v-card-actions>
+        <v-btn outlined rounded text @mousedown="spin()"> Spin </v-btn>
+      </v-card-actions>
+    </div>
   </v-card>
 </template>
 
 <script>
-export default {};
+import Reels from "@/components/SlotMachine/SlotReels/SlotReels.vue";
+
+export default {
+  components: {
+    Reels,
+  },
+  data: () => ({
+    animate: false,
+    currentBalance: 0,
+    payTable: [
+      {
+        position: "top",
+        combination: ["Cherry", "Cherry", "Cherry"],
+        value: 2000,
+      },
+      {
+        position: "center",
+        combination: ["Cherry", "Cherry", "Cherry"],
+        value: 1000,
+      },
+      {
+        position: "bottom",
+        combination: ["Cherry", "Cherry", "Cherry"],
+        value: 4000,
+      },
+      {
+        position: null,
+        combination: ["7", "7", "7"],
+        value: 150,
+      },
+      {
+        position: null,
+        combination: ["7", "Cherry"],
+        value: 75,
+      },
+      {
+        position: null,
+        combination: ["3xBAR", "3xBAR", "3xBAR"],
+        value: 50,
+      },
+      {
+        position: null,
+        combination: ["2xBAR", "2xBAR", "2xBAR"],
+        value: 20,
+      },
+      {
+        position: null,
+        combination: ["BAR", "BAR", "BAR"],
+        value: 10,
+      },
+      {
+        position: null,
+        combination: ["BAR", "2xBAR", "3xBAR"],
+        value: 5,
+      },
+    ],
+    results: [],
+  }),
+  methods: {
+    spin() {
+      this.results = [];
+      this.animate = true;
+      this.$refs.reel1.start();
+      this.$refs.reel2.start();
+      this.$refs.reel3.start();
+    },
+    reelStopped(resultData) {
+      this.results.push(resultData);
+      if (this.results.length === 3) {
+        this.checkWin(this.results);
+      }
+    },
+    checkWin(data) {
+      // console.log(data);
+    },
+  },
+};
 </script>
 
 <style>
 .SlotMachine {
   border: 2px solid black;
   border-radius: 5px;
-  width: 300px;
-  height: 300px;
+  overflow: hidden;
+  height: 455px;
 }
 </style>
--- src/components/SlotMachine/SlotReels/SlotReels.vue
+++ src/components/SlotMachine/SlotReels/SlotReels.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="Reel">
-    <div class="Reel-wrapper" v-for="n in 5" :key="n">
-      <img class="Reel-image" :src="reelTileData[n].image" />
+  <div class="Reel" :class="animate ? 'move' : ''">
+    <div class="Reel-wrapper d-flex flex-column mb-6">
+      <img
+        v-for="(item, index) in shuffledReelTileData"
+        :key="index"
+        class="Reel-image mb-3 pa-3"
+        :id="index"
+        :src="`${item.image}`"
+      />
     </div>
   </div>
 </template>
@@ -9,81 +15,30 @@
 <script>
 export default {
   data: () => ({
+    animate: false,
     speed: null,
-    reelTileData: null,
-    reelIndex: 2,
-    tile1Index: 0,
-    tile2Index: 1,
-    tile3Index: 2,
-    tile4Index: 3,
-    tile5Index: 4,
+    reelTileData: [],
+    reelIndex: 1,
     reelData: [
       {
         name: "3xBAR",
-        image: "@/assets/3xBAR.png",
+        image: "/assets/3xBAR.png",
       },
       {
         name: "2xBAR",
-        image: "@/assets/2xBAR.png",
+        image: "/assets/2xBAR.png",
       },
       {
         name: "BAR",
-        image: "@/assets/BAR.png",
+        image: "/assets/BAR.png",
       },
       {
         name: "7",
-        image: "@/assets/7.png",
+        image: "/assets/7.png",
       },
       {
         name: "Cherry",
-        image: "@/assets/Cherry.png",
-      },
-    ],
-    payTable: [
-      {
-        position: "top",
-        combination: ["Cherry", "Cherry", "Cherry"],
-        value: 2000,
-      },
-      {
-        position: "center",
-        combination: ["Cherry", "Cherry", "Cherry"],
-        value: 1000,
-      },
-      {
-        position: "bottom",
-        combination: ["Cherry", "Cherry", "Cherry"],
-        value: 4000,
-      },
-      {
-        position: null,
-        combination: ["7", "7", "7"],
-        value: 150,
-      },
-      {
-        position: null,
-        combination: ["7", "Cherry"],
-        value: 75,
-      },
-      {
-        position: null,
-        combination: ["3xBAR", "3xBAR", "3xBAR"],
-        value: 50,
-      },
-      {
-        position: null,
-        combination: ["2xBAR", "2xBAR", "2xBAR"],
-        value: 20,
-      },
-      {
-        position: null,
-        combination: ["BAR", "BAR", "BAR"],
-        value: 10,
-      },
-      {
-        position: null,
-        combination: ["BAR", "2xBAR", "3xBAR"],
-        value: 5,
+        image: "/assets/Cherry.png",
       },
     ],
   }),
@@ -91,73 +46,75 @@ export default {
     let symbolsArray = [];
     let count = this.reelData.length * 3;
 
+    // Create bigger pool of symbols for reels
     while (count > 0) {
       const symbolIndex = Math.floor(Math.random() * this.reelData.length);
       const symbolObject = this.reelData[symbolIndex];
       symbolsArray.push(symbolObject);
       count--;
     }
-
-    this.reelTileData = this.shuffleIndexOfReelSymbols(symbolsArray);
+    // Set reelTileData array to the bigger pool
+    this.reelTileData = symbolsArray;
   },
   mounted: function () {
+    // Once transition timing ends the animateEnd method is called to handle the passing
+    // of data to the parent component
     this.$el.addEventListener("transitionend", this.animateEnd);
   },
   methods: {
-    shuffleIndexOfReelSymbols(array) {
-      var currentIndex = array.length,
-        temporaryValue,
-        randomIndex;
+    shuffleArray(array) {
+      // Fisher Yates Shuffle method pretty much unbias shuffling of array
+      const a = array.slice();
 
-      // While there remain elements to shuffle...
-      while (0 !== currentIndex) {
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+      for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
       }
-
-      return array;
+      return a;
     },
     start() {
       const min = 8;
       const max = 28;
       const speed = Math.floor(Math.random() * (max - min + 1) + min);
       this.speed = speed;
-      this.animate();
+      this.toggleAnimate();
     },
-    animate() {
-      this.$el.classList.add("move");
+    toggleAnimate() {
+      this.animate = true;
+      this.reelTileData = this.shuffleArray(this.reelTileData);
     },
     animateEnd() {
-      this.$el.classList.remove("move");
-      this.reIndex();
+      this.animate = false;
       this.speed = this.speed - 1;
       if (this.speed > 0) {
-        setTimeout(this.animate, 10);
+        setTimeout(this.toggleAnimate, 10);
       } else {
-        const reelTileData = this.reelTileData[this.reelIndex];
-        this.$emit("stopped", reelTileData);
+        const topBar = this.shuffledReelTileData[0];
+        const middleBar = this.shuffledReelTileData[1];
+        const bottomBar = this.shuffledReelTileData[2];
+
+        this.$emit("stopped", middleBar);
       }
     },
-    reIndex() {
-      const end = this.reelTileData.length - 1; //this.reelTileCount - 1
-      const index = this.reelIndex === 0 ? end : this.reelIndex - 1;
-      // const index = this.index === end ? 0 : this.index + 1
-      this.reelIndex = index;
-      this.tile1Index = index === 1 ? end : index === 0 ? end - 1 : index - 2;
-      this.tile2Index = index === 0 ? end : index - 1;
-      this.tile3Index = index;
-      this.tile4Index = index === end ? 0 : index + 1;
-      this.tile5Index = index === end - 1 ? 0 : index === end ? 1 : index + 2;
+    randomIndex() {
+      return Math.floor(Math.random() * (15 - 0) + 0);
+    },
+  },
+  computed: {
+    shuffledReelTileData() {
+      return this.shuffleArray(this.reelTileData);
     },
   },
 };
 </script>
 
-<style>
-</style>
+<style lang="postcss">
+:root {
+  --tileSize: 90px;
+}
+
+.Reel.move .Reel-wrapper {
+  transition: margin-top 0.04s linear;
+  margin-top: var(--tileSize);
+}
+</style>>
