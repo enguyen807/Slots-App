36ba9e4af5f09935b1cd9591bb455b87e0e3b062
Miles Le <eric.nguyen@fleishman.com>
2021-05-12 01:18:18 -0500

    Add base layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app
+    id="main"
+    :style="{ background: this.$vuetify.theme.themes[theme].background }"
+  >
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-toolbar-title>Slot Machine Test</v-toolbar-title>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-layout row wrap>
+          <v-col class="fill-height">
+            <v-sheet min-height="70vh" rounded="lg" elevation="12">
+              <slot></slot>
+            </v-sheet>
+          </v-col>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+  components: {},
+  methods: {},
+  computed: {
+    theme() {
+      return this.$vuetify.theme.dark ? "dark" : "light";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import colors from "vuetify/lib/util/colors";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.orange.darken1,
+        secondary: colors.orange.lighten4,
+        accent: colors.lightGreen.lighten2,
+        background: colors.grey.lighten3,
+      },
+    },
+  },
+});
